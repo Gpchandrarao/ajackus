2b84c6c80c3fee4986d6a64e9dcc08da583b8ac0
Gpchandrarao <gandikotapoornachandrarao@gmail.com>
2025-02-08 12:19:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import User from "./pages/User";
+
+const App = () => {
+  return (
+    <div className="">
+      <User />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/User.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div>
+      <h1></h1>User
+    </div>
+  );
+};
+
+export default User;
